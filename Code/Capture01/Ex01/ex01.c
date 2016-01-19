b2db09feff3e683c8e4f51c21674fbd19ba5128b
root <root@localhost.localdomain>
2016-01-20 03:16:49 +0800

    finish tapy the code

--- Code/Capture01/Ex01/ex01.c
+++ Code/Capture01/Ex01/ex01.c
@@ -20,7 +20,7 @@ void rearrange( char *output, char const *input, int n_columns, int const colnum
 
 int main( void ){
 
-	int n_column; //the number of column is dealing  with
+	int n_columns; //the number of column is dealing  with
 	int columns[MAX_COLS]; //the number of columns needs to deal with
 	char input[MAX_INPUT]; //the max number of line can be dealed
 	char output[MAX_INPUT]; //the max of output
@@ -28,10 +28,77 @@ int main( void ){
 	// get current column number	
 	n_columns = read_column_numbers( columns, MAX_COLS );
 
-	//
+	//read,deal and print last input
+	while(gets( input ) != NULL){
+		printf("Original input : %s\n",input);
+		rearrange( output, input, n_columns, columns);
+		printf("Rearranged line: %s\n",output);
+	}
 
+	return EXIT_SUCCESS;
 
+}
 
+/*
+ *get the rangement of the column, ignore the overflued value
+ * 
+ */
+int read_column_numbers( int columns[], int max ){
+	int num = 0;
+	int ch;
 
+	//get column,end with negative number
+	while( num < max && scanf("%d", &columns[num]) == 1 && columns[num] >= 0){
+		num += 1;
+	}
+
+	//check the number are coupled
+	if( num % 2 != 0 ){
+		puts("Last column number is not paired.");
+		exit( EXIT_FAILURE );
+	}
+
+	//abonden the unusefuled part
+	while((ch = getchar()) != EOF && ch != '\n');
+
+	return num;
+}
+
+/*
+ *deal with the input and link the chars ,ending it with NUL
+ *
+ * */
+void rearrange( char *output, char const *input, int n_columns, int const columns[] ){
 	
-} 
+	int col; //column pointer
+	int output_col; //output columns counter
+	int len; //length of input 
+
+	len = strlen( input );
+	output_col = 0;
+
+	//operate coupled columns
+	for( col =0; col < n_columns; col +=2 ){
+		int nchars = columns[col + 1] - columns[col] + 1;
+
+		//end it up if input line is ended or output line is fulled
+		if( columns[col] >= len || output_col == MAX_INPUT - 1) break;
+
+		//abonden voerflow part
+		if( output_col + nchars > MAX_INPUT - 1 )
+			nchars = MAX_INPUT - output_col - 1;
+
+		//copy data
+		strncpy( output + output_col, input + columns[col], nchars );
+		output_col += nchars;
+	}
+	output[output_col] = '\0';
+}
+
+
+
+
+
+
+
+
